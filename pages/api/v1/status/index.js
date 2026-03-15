53c56bf4e24520932dbb7f5dc090442f93a3a71d
maurocoimbra <maurocoimbrab@gmail.com>
2026-03-15 15:00:18 -0300

    add database.js and update index.js to use it

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function health(request, response) {
+import database from "../../../../infra/database.js";
+
+async function health(request, response) {
+  const result = await database.query("SELECT 1 + 1 AS sum;");
+  console.log(result.rows);
   response.status(200).json({ status: "ok" });
 }
 
