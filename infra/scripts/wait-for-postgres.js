c7e78a09740779ee871bd8610b4f236939711a46
maurocoimbra <maurocoimbrab@gmail.com>
2026-04-06 14:14:24 -0300

    makes npm test more robust with async-retry and orchestrator.js

--- infra/scripts/wait-for-postgres.js
+++ infra/scripts/wait-for-postgres.js
@@ -10,7 +10,7 @@ function checkPostgres() {
       return;
     }
 
-    console.log("\nPostgres is ready and accepting connections");
+    console.log("\nPostgres is ready and accepting connection\n");
   }
 }
 
