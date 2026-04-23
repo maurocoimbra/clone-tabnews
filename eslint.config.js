0ba12a0dd002bcba34ec183a16a1172fbf5fd53a
maurocoimbra <maurocoimbrab@gmail.com>
2026-04-22 23:23:22 -0300

    add jsx to eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,11 @@ const { defineConfig } = require("eslint/config");
 eslintConfig = defineConfig([
   {
     files: ["**/*.js", "**/*.cjs", "**/*.mjs"],
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+      },
+    },
     rules: {
       "prefer-const": "warn",
       "no-constant-binary-expression": "error",
